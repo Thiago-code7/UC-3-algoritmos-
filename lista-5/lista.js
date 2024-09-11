5876ea7ae75406259c458ccd306ef27609c36515
Thiago-code7 <monteirothiago719@gmail.com>
2024-09-11 17:05:52 -0300

    add: atividade lista 05

---
+++ lista-5/lista.js
@@ -0,0 +1,311 @@
+// Questão 1
+
+ // let frutas = ['maca', 'banana', 'laranja'];
+// // frutas.unshift('abacaxi'); 
+// // console.log(frutas);
+// // frutas.push('manga');
+// // console.log(frutas);
+// // const removeFruta = frutas.shift();
+// // console.log(frutas);
+// // const removeUltimaFruta = frutas.pop();
+// // console.log(frutas);
+
+// // Questão 2
+
+// let numeros = [10, 20, 30, 40, 50];
+
+// // 1- 
+
+// // numeros.splice(2, 0, 25)
+// // console.log(numeros);
+
+// // // 2- 
+
+// // numeros.splice(4,1);
+// // console.log(numeros);
+
+// // Questão 3
+
+// //  let cores = ['vermelho', 'azul', 'verde', 'amarelo',
+// //     'azul'];
+
+// // 1-
+
+// // cores.indexOf('azul');
+// // console.log(cores.indexOf('azul'));
+
+// // // 2-
+
+// // cores.lastIndexOf('azul');
+// // console.log(cores.lastIndexOf('azul'));
+
+// // // 3- 
+// // cores.includes('roxo');
+// // console.log(cores.includes('roxo'));
+
+// // Questão 4
+
+// // let alunos = ['João', 'Ana', 'Carlos', 'Bia'];
+
+// // 1-
+
+// // alunos.splice(2, 1);
+
+// // console.log(alunos);
+
+// // // 2-
+
+// // alunos.push('Maria');
+// // console.log(alunos);
+
+// // // 3-
+
+// // alunos.unshift('Pedro');
+// // console.log(alunos);
+
+// // Questão 5
+
+// // let animais = ['gato', 'cachorro', 'pássaro', 'peixe',
+// //     'gato'];
+
+// //     // 1- 
+
+// //     animais.shift();
+
+// //     console.log(animais);
+
+// //     // 2- 
+
+// //     animais.push('hamster');
+// //     console.log(animais);
+
+// //     // 3-
+
+// //     animais.splice(1,1);
+// //     console.log(animais);
+
+// //     // 4-
+
+// //     console.log(animais.includes('gato'));
+
+//     // Questão 6
+
+// // let livros = ['O Senhor dos Anéis', '1984', 'O Pequeno Príncipe', 'Harry Potter'];
+
+// // // 1- 
+
+// // console.log(livros.indexOf('1984'));
+
+// // // 2- 
+
+// // livros.splice(2,1);
+// // console.log(livros);
+
+// // Questão 7
+
+// // let cidades = ['São Paulo', 'Rio de Janeiro', 'Salvador',
+// //     'Brasília'];
+
+// //     // 1-
+// //     console.log(cidades.includes('Salvador'));
+// //     // 2-
+// //     cidades.unshift('Curitiba');
+// //     console.log(cidades);
+
+//     // Questão 8
+
+//     // let jogos = ['FIFA', 'Minecraft', 'Fortnite', 'Among Us'];
+
+//     // // 1-
+
+//     // jogos.splice(1,1);
+//     // jogos.splice(1, 0, 'The Sims');
+
+//     // console.log(jogos);
+
+//     // // 2- 
+
+//     // jogos.shift();
+//     // console.log(jogos);
+
+//     // Questão 9
+
+//     // let esportes = ['futebol', 'basquete', 'vôlei',
+//     //     'tênis'];
+
+//     //     // 1-
+
+//     // esportes.shift();
+
+//     // console.log(esportes);
+
+//     // // 2-
+
+//     // console.log(esportes.includes('vôlei'));
+
+//     // // 3-
+
+//     // esportes.splice(1, 1);
+//     // console.log(esportes);
+
+//     // Questão 10
+
+//     // let tecnologias = ['HTML', 'CSS', 'JavaScript',
+//     //     'React'];
+//     // // 1-
+
+//     // tecnologias.pop();
+//     // console.log(tecnologias);
+
+//     // // 2-
+
+//     // tecnologias.unshift('Node.js');
+//     // console.log(tecnologias);
+
+//     // // 3- 
+
+//     // console.log(tecnologias.indexOf('JavaScript'));
+
+//     // Questão 11
+
+//     // let filmes = ['Matrix', 'Inception', 'Avatar'];
+
+//     // // 1-
+//     // filmes.unshift('interstellar');
+//     // console.log(filmes);
+
+//     // // 2- 
+//     // filmes.pop();
+//     // console.log(filmes);
+
+//     // // 3-
+
+//     // filmes.push('The Godfather');
+//     // console.log(filmes);
+
+//     // Questão 12
+
+//     // let cores = ['vermelho', 'azul', 'verde',
+//     //     'amarelo'];
+
+//     // cores.splice(2, 1);
+//     // console.log(cores);
+    
+//     // // 2
+
+//     // cores.splice(2, 0, 'roxo');
+//     // console.log(cores);
+
+//     // Questão 13
+
+//     // let times = ['Flamengo', 'Corinthians',
+//     //     'Palmeiras'];
+
+//     //     console.log(times.includes('São Paulo'));
+
+//     //     times.push('São Paulo');
+//     //     console.log(times);
+
+//         // Questão 14
+
+//     // 1-
+
+//     // let comidas = ['pizza', 'hamburguer', 'sushi',
+//     //     'lasanha'];
+
+//     //     // 1-
+
+//     //     console.log(comidas.indexOf('sushi'));
+
+//     //     // 2-
+
+//     //     comidas.splice(2, 1);
+
+//     //     console.log(comidas);
+
+//         // Questão 15
+
+//         let numeros2 = [5, 10, 15, 20];
+
+//     // 1- 
+
+//     numeros2.push(25);
+//     console.log(numeros2);
+
+//     // 2
+
+//     numeros2.splice(1,1);
+//     console.log(numeros2);
+//     // 3
+
+//     numeros2.unshift(7);
+//     console.log(numeros2);
+
+// Questão 16
+
+// let planetas = ['Terra', 'Marte', 'Júpiter',
+//     'Saturno'];
+
+// // 1-
+
+// planetas.shift();
+// console.log(planetas);
+
+// // 2-
+
+// planetas.unshift('Vênus');
+// console.log(planetas);
+
+// Questão 17
+
+// cursos = ['Matemática', 'Física', 'Química',
+//     'Biologia'];
+
+//     // 1-
+
+//     console.log(cursos.includes('Física'));
+//     // 2-
+//     cursos.splice(1,1);
+//     console.log(cursos);
+
+    // Questão 18
+
+    // let carros = ['Ferrari', 'Lamborghini', 'Porsche',
+    //     'Bugatti'];
+
+    // // 1- 
+
+    // carros.splice(1,1);
+    // carros.splice(1,0,"McLaren");
+    // console.log(carros);
+
+// Questão 19
+
+let idiomas = ['Inglês', 'Espanhol', 'Francês',
+    'Alemão'];
+
+    // 1-
+
+    idiomas.shift();
+    console.log(idiomas);
+
+    // 2- 
+
+    idiomas.push('italiano');
+    console.log(idiomas);
+
+    // 3-
+
+    console.log(idiomas.indexOf('Francês'));
+
+// Questão 20
+
+let frutas = ['maçã', 'banana', 'laranja', 'uva'];
+
+frutas.unshift('morango');
+console.log(frutas);
+
+// 2-
+
+console.log(frutas.includes('laranja'));
+
